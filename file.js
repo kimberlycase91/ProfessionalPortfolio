16016d66337e2926be3a077d239ebf6c88d663f0
kimberlycase91 <kimberlycase91@gmail.com>
2020-05-20 11:08:58 -0700

    added and removed some projects

---
+++ file.js
@@ -0,0 +1,125 @@
+<div class="col s6">
+<div class="portfolio-link">
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row no-gutters">
+            <div class="col s4">
+                <a target="_blank" href="https://github.com/kimberlycase91/Project-01">
+                    <img src="assets/images/eventSearch.JPG" class="card-img"
+                        alt="eventSearchApp">
+                </a>
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Event Search</h5>
+                    <p class="card-text">An web application that utilizes the Eventful API
+                        and
+                        Google
+                        Maps API to allow the user to search for </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="portfolio-link">
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row no-gutters">
+            <div class="col s4">
+                <a target="_blank" href="https://github.com/kimberlycase91/google-book-search">
+                    <img src="assets/images/book-search.png" class="card-img"
+                        alt="book search app">
+                </a>
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Google Book Search</h5>
+                    <p class="card-text">A MERN app that uses React components, with helper/util functions, and utilizes React lifecycle methods to query and display books based on user searches.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="portfolio-link">
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row no-gutters">
+            <div class="col s4">
+                <a target="_blank" href="https://github.com/kimberlycase91/In-A-Giphy">
+                    <img src="assets/images/giphySearch.JPG" class="card-img"
+                        alt="giphy search app">
+                </a>
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Giphy Search</h5>
+                    <p class="card-text">Web application that uses the Giphy API to
+                        dynamically display relevant gifs and their ratings based on the
+                        button the user clicks.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="col s6">
+<div class="portfolio-link">
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row no-gutters">
+            <div class="col s4">
+                <a target="_blank" href="https://github.com/kimberlycase91/TriviaGame">
+                    <img src="assets/images/princessQuiz.JPG" class="card-img"
+                        alt="princess quiz app">
+                </a>
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Princess Quiz</h5>
+                    <p class="card-text">Web application that utilizes jquery and moment.js
+                        in a dynamically created timed quiz.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="portfolio-link">
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row no-gutters">
+            <div class="col s4">
+                <a target="_blank" href="https://github.com/kimberlycase91/react-memory-game">
+                    <img src="assets/images/princess-memory.png" class="card-img"
+                        alt="princess memeory game">
+                </a>
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Princess Memory Game</h5>
+                    <p class="card-text">A memory game made with React that break up the application's UI into components, manage component state, and respond to user events.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="portfolio-link">
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row no-gutters">
+            <div class="col s4">
+                <a target="_blank" href="https://github.com/kimberlycase91/friend-finder">
+                    <img src="assets/images/friendFinder.JPG" class="card-img"
+                        alt="friend finder app">
+                </a>
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Princess Friend Finder</h5>
+                    <p class="card-text">A compatibility-based 'friend finder' app that uses
+                        node.js, express, path, and materialize to find a user's recommended
+                        princess best friend.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
